256ea34e692c243342d7e951b541c7f53d02738d
nubmast <nubmaster2407@gmail.com>
2026-08-16 16:19:53 -0400

    EJERICICIO N1 LISTO G2

--- GUIA DE EJERCICIOS 2/N1/areadeuntriangulo.c
+++ GUIA DE EJERCICIOS 2/N1/areadeuntriangulo.c
@@ -6,11 +6,53 @@
 // ELABORADO POR JUAN MEDINA 16/08/2026
 
 /* 
-
+    Escribir un programa que calcule el área de un triángulo, para ello se debe solicitar al usuario la base 
+    y la altura del triángulo, posteriormente se debe calcular el área y mostrarla en pantalla.
 */
 
 void limpiarBuffer() 
 {
     int c;
     while ((c = getchar()) != '\n' && c != EOF);
+}
+
+int main() 
+{
+    long double base, altura, area;
+    do 
+    {
+        system("cls");
+        printf("Ingrese la base del triángulo (mayor que 0): ");
+        if (scanf("%Lf", &base) != 1 || base <= 0) 
+        {
+            printf("Error: Dato invalido, solo numeros mayores a cero.\n");
+            limpiarBuffer();
+            sleep(3);
+            continue;
+        }
+        limpiarBuffer();
+        break;
+    } while (1);
+    
+    do 
+    {
+        system("cls");
+        printf("Ingrese la altura del triángulo (mayor que 0): ");
+        if (scanf("%Lf", &altura) != 1 || altura <= 0) 
+        {
+            printf("Error: Dato invalido, solo numeros mayores a cero.\n");
+            limpiarBuffer();
+            sleep(3);
+            continue;
+        }
+        limpiarBuffer();
+        break;
+    } while (1);
+    
+    area = (base * altura) / 2;
+
+    system("cls");
+    printf("El área del triángulo es: %.2Lf\n", area);
+
+    sleep(10);
 }
--- GUIA DE EJERCICIOS 2/N3/masacorporal.c
+++ GUIA DE EJERCICIOS 2/N3/masacorporal.c
@@ -14,3 +14,4 @@ void limpiarBuffer()
     int c;
     while ((c = getchar()) != '\n' && c != EOF);
 }
+
