b711dd21f91eca0b50af4cbbe5c2503af0f5c62e
nubmast <nubmaster2407@gmail.com>
2026-08-16 18:48:32 -0400

    .

---
+++ GUIA DE EJERCICIOS 2/N7/angulos.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <ctype.h>
+#include <math.h>
+
+// ELABORADO POR JUAN MEDINA 16/08/2026
+
+/*
+
+
+*/
+
+void limpiarBuffer() 
+{
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF);
+}
