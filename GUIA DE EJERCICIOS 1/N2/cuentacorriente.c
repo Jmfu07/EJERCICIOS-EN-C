05e2db6c658b25ac38f6c3dde1e7fcc8387f98f6
Jmfu07 <jmdearmas07@gmail.com>
2026-08-15 20:52:57 -0400

    progreso

--- GUIA DE EJERCICIOS 1/N2/cuentacorriente.c
+++ GUIA DE EJERCICIOS 1/N2/cuentacorriente.c
@@ -7,21 +7,20 @@
 El programa debe pedir una clave no mayor a 4 digitos, el usuario debe poder ingresar que opcion 
 desea realizar como pedir su saldo y este ser impreso en la pantalla, todo debe estar validado para:
 
-- consulta de saldo
-- deposito
-- retiro
+1- consulta de saldo
+2- deposito
+3- retiro
 
 No puedes retirar mas de lo que tienes
 No puedes depositar mas de lo que tienes
 
 */
-
 void menu ()
 {
-    printf ("BIENVENIDO AL BFC");
-    printf ("1. Consulta de saldo");
-    printf ("2. Deposito ");
-    printf ("3. Retiro");
+    printf ("BIENVENIDO AL BFC\n");
+    printf ("1. Consulta de saldo\n");
+    printf ("2. Deposito \n");
+    printf ("3. Retiro\n");
 }
 
 // Función para vaciar letras del búfer
@@ -32,14 +31,43 @@ void limpiarBuffer() {
 
 int validacionmenu(int op1)
 {
-    return (op1 > 0 && op1 < 3);
+    return (op1 > 0 && op1 <= 3 );
 }
 
-
-
-int main 
+int validacioncontr (int op2)
 {
 
-    int saldo=0, deposito=0, retiro=0; op1=0, op2=0; 
+    
+}
 
+int main()
+{
+
+    int saldo=0, deposito=0, retiro=0, op1=0, op2=0; 
+    menu();
+    do  
+    {
+        printf("\nIngrese su opcion: ");
+   
+        // Si scanf devuelve 0, el usuario escribió una letra
+        if (scanf("%d", &op1) != 1) 
+            {
+                printf("Error: Solo se permiten numeros.\n");
+                limpiarBuffer(); // Limpia la letra atrapada para evitar el bucle infinito
+                op1 = -1;     // Asignamos un valor inválido para forzar a repetirse
+            } 
+            else  
+            {
+                limpiarBuffer(); // Limpia el 'Enter' sobrante
+            }
+
+    } while (!validacionmenu(op1));
+
+
+
+
+    if (op1 == 1)
+    {
+        printf ("Su saldo es de: %d ", saldo);
+    }
 }
