61c2cb5a5f884e65f22ac0d89c3553385b5587fa
Jmfu07 <jmdearmas07@gmail.com>
2026-08-17 00:11:51 -0400

    EJERICICIO N9 LISTO G2

--- GUIA DE EJERCICIOS 2/N9/notaexamen.c
+++ GUIA DE EJERCICIOS 2/N9/notaexamen.c
@@ -13,9 +13,53 @@ APROBADO si esta entre 5 y 7 sin incluir, NOTABLE si esta entre 7 y 9 sin inclui
 entre 9 y 10 sin incluir y MATRICULA DE HONOR si la nota es igual a 10.
 
 */
-
+void limpiarbuffer();
 void limpiarBuffer() 
 {
     int c;
     while ((c = getchar()) != '\n' && c != EOF);
+}
+
+int main ()
+{
+    double nota;
+
+    do 
+    {
+        system("cls");
+        printf("Ingrese su nota: ");
+        if (scanf("%lf", &nota) != 1 || nota < 0 || nota > 10) 
+        {
+            printf("Error: Dato invalido, solo numeros mayores a cero.\n");
+            limpiarBuffer();
+            sleep(3);
+            continue;
+        }
+        limpiarBuffer();
+        break;
+
+    } while (1);
+
+    if (nota < 5.0)
+    {
+        printf ("SUSPENDIDO");
+    }
+    else if (nota < 7.0)
+    {
+        printf ("APROBADO");
+    }
+    else if (nota < 9.0)
+    {
+        printf ("NOTABLE");
+    }
+    else if (nota < 10)
+    {
+        printf ("SOBRESALIENTE");
+    }
+    else if (nota == 10)
+    {
+        printf ("MATRICULA DE HONOR");
+    }
+
+    sleep(5);
 }
