aed5585267deaabdf7b8f33e485c68e5f1983589
Jmfu07 <jmdearmas07@gmail.com>
2026-08-20 21:16:36 -0400

    .

---
+++ GUIA DE EJERCICIOS 3/N1/nacisteel.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <ctype.h>
+#include <math.h>
+
+// ELABORADO POR JUAN MEDINA 20/08/2026
+
+/*
+
+escribir programa que de un mensaje de saludo y luego muestre un mensaje que digga "Porfavor introduzca el año en que nacio"
+luego el programa debe soltar ¨Si usted nacio en (fecha) este año cumple (edad calculada) años¨
+
+*/
+
+void limpiarBuffer() 
+{
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF);
+    
+}
